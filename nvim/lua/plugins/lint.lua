20afc9c79fb569231eb8260db45ed80ee1c2398a
marvindjr <dev@marvindore.com>
2024-11-19 14:15:13 -0500

    Configure cspell toggle

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -9,7 +9,12 @@ vim.api.nvim_create_autocmd({"BufWritePost", "BufEnter"}, {
   group = vim.api.nvim_create_augroup('lint', { clear = true }),
   callback = function()
     lint.try_lint()
-    lint.try_lint("cspell")
+    --local on_demand_linters = {"cspell"}
+    local function lint_cspell()
+       lint.try_lint({"cspell"})
+    end
+    vim.keymap.set("n", "<leader>cll", lint_cspell, { desc = "Cspell Lint On"})
+    vim.keymap.set("n", "<leader>cln", vim.diagnostic.reset, { desc = "Cspell Lint Off"})
   end
 })
 
