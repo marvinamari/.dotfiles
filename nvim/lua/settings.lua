1aef2fab50cea3715b2b17d22fc90f55611b843b
marvindjr <dev@marvindore.com>
2024-12-03 10:56:22 -0500

    Enable clipboard in wsl

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -178,3 +178,20 @@ end
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
   callback = update_winbar,
 })
+
+-- WSL
+local clip = "/mnt/c/Windows/System32/clip.exe" -- Change this path if needed
+
+if vim.fn.executable(clip) then
+  local opts = {
+    callback = function()
+      if vim.v.event.operator ~= "y" then
+        return
+      end
+      vim.fn.system(clip, vim.fn.getreg(0))
+    end,
+  }
+
+  opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+  vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+end
